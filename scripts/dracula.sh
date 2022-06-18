75dcbbabfa4c2a38e29e29a39a8bf09efd3fd070
Aaron Kollasch <aaron@kollasch.dev>
2022-06-18 05:35:23 -0400

    Add attached-clients plugin
    (cherry picked from commit 3e75296b73f483806291f4f1c5ff4bb09a8ad465)

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -176,14 +176,9 @@ main()
       script="#($current_dir/network_ping.sh)"
     fi
 
-    if [ $plugin = "spotify-tui" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-spotify-tui-colors" "green dark_gray")
-      script="#($current_dir/spotify-tui.sh)"
-    fi
-
-    if [ $plugin = "kubernetes-context" ]; then
-      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-kubernetes-context-colors" "cyan dark_gray")
-      script="#($current_dir/kubernetes_context.sh $show_kubernetes_context_label)"
+    if [ $plugin = "attached-clients" ]; then
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-attached-clients-colors" "cyan dark_gray")
+      script="#($current_dir/attached_clients.sh)"
     fi
 
     if [ $plugin = "weather" ]; then
