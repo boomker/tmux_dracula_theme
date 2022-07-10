d03fc1dc8a93fd66b777e38faacbf34fd376e707
pataquets <amontero@tinet.org>
2022-07-10 19:51:32 +0200

    Add 'custom' status plugin to get status texts from external scripts.
    (cherry picked from commit 44018dede10404f1c3115f8aa8bacacced47c2d0)

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -134,6 +134,18 @@ main()
         script="#($current_dir/job.sh)"
     fi
 
+    if case $plugin in custom:*) true;; *) false;; esac; then
+      script=${plugin#"custom:"}
+      if [[ -x "${current_dir}/${script}" ]]; then
+        IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-custom-plugin-colors" "cyan dark_gray")
+        script="#($current_dir/${script})"
+      else
+        colors[0]="red"
+        colors[1]="dark_gray"
+        script="${script} not found!"
+      fi
+    fi
+
     if [ $plugin = "git" ]; then
       IFS=' ' read -r -a colors  <<< $(get_tmux_option "@dracula-git-colors" "green dark_gray")
       tmux set-option -g status-right-length 250
